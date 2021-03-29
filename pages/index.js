b39c0240d3b76bcb83091fe00b135bfec274e2e9
gokozdemir <gokozdemir20@gmail.com>
2021-03-29 23:58:38 +0300

    layouts has been created

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function HomePage() {
-    return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
-  }
-  
-  export default HomePage
+    return (
+        <>
+            <div className="site-container">
+                <div className="space-y-4">
+                    <h1 className="text-2xl font-bold">Merhaba ben Gökhan Özdemir</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident consequuntur veritatis laudantium, aperiam ducimus accusantium fugit nesciunt commodi modi ex, libero expedita pariatur ullam voluptatum voluptate. Incidunt, quaerat dolorum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa provident consequuntur veritatis laudantium, aperiam ducimus accusantium fugit nesciunt commodi modi ex, libero expedita pariatur ullam voluptatum voluptate. Incidunt, quaerat dolorum!</p>
+                </div>
+            </div>
+            <div className="max-w-4xl mx-auto mt-10">
+                <img src="/profil-resmi.jpg" alt="" />
+            </div>
+        </>
+
+    )
+}
+
+export default HomePage
