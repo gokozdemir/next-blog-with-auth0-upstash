32d82e01cae0f5a9de9691d6ee54c94ef4c342a4
gokozdemir <gokozdemir20@gmail.com>
2021-04-03 16:43:41 +0300

    Auth system added

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import '../styles/global.css'
 import Header from '../components/header'
+import { Auth0Provider } from "@auth0/auth0-react"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased text-gray-700">
-      <Header />
-      <main className="mt-6 mb-20">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain="dev-oa6mzijr.us.auth0.com"
+      clientId="ta0C9cZDCov9YRJiTu3LlLZF43Ea38JO"
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="antialiased text-gray-700">
+        <Header />
+        <main className="mt-6 mb-20">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
+
   )
 }
 
